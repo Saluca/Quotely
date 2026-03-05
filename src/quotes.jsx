1c664ab28d0cd66783032dd177e3e9915d47ff5d
Saluca <Saluca@users.noreply.github.com>
2026-03-05 16:09:19 +0000

    Initial commit

---
+++ src/quotes.jsx
@@ -0,0 +1,22 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { useState } from "react";
+
+export default function Quotes() {
+  const [quote, setQuote] = useState(null);
+
+  async function getQuote() {
+    const response = await getDocs(collection(db, "quotes"));
+    const quotes = response.docs.map((doc) => doc.data());
+    const random = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(random);
+  }
+
+  return (
+    <div>
+      <button onClick={getQuote}>Get Quote</button>
+      {quote && <p>{quote.quote}</p>}
+      {quote && <p>{quote.author}</p>}
+    </div>
+  );
+}
