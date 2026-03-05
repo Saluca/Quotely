d9d1c3528e19d00e813a8909dd64d2f5b2f9dc89
Saluca <Saluca@users.noreply.github.com>
2026-03-05 21:20:56 +0000

    Added a copy functionality and a CSS for the copy button.Added placeholder text for initial mount

--- src/quotes.jsx
+++ src/quotes.jsx
@@ -4,13 +4,22 @@ import { useState } from "react";
 import "./quotes.css";
 
 export default function Quotes() {
-  const [quote, setQuote] = useState(null);
-
+  const [quote, setQuote] = useState({
+    quote: "Click the button for a quote...",
+    author: "Quotely",
+  });
+  const [copied, setCopied] = useState(false);
   async function getQuote() {
     const response = await getDocs(collection(db, "quotes"));
     const quotes = response.docs.map((doc) => doc.data());
     const random = quotes[Math.floor(Math.random() * quotes.length)];
     setQuote(random);
+  }
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(quote.quote);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2000);
   }
 
   return (
@@ -26,6 +35,9 @@ export default function Quotes() {
               <p className="quote-author"> - {quote.author}</p>
             </>
           )}
+          <button onClick={copyToClipboard} className="copy-btn">
+            {copied ? "Copied!" : "Copy Quote"}
+          </button>
         </div>
       </div>
     </div>
