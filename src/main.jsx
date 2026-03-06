652ab66d120b6b9c27efb314e3e531bf13b12684
Saluca <Saluca@users.noreply.github.com>
2026-03-06 17:13:01 +0000

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Quotes from "./quotes.jsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Quotes />
+    <Analytics />
   </StrictMode>,
 );
